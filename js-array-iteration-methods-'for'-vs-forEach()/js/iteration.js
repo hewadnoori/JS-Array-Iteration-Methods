b00840f61711ddf4dd360bdb4e8a9a34280e53ee
Hewad Noori <hewadnoori16@gmail.com>
2022-05-04 14:02:13 -0400

    map method

--- js-array-iteration-methods-'for'-vs-forEach()/js/iteration.js
+++ js-array-iteration-methods-'for'-vs-forEach()/js/iteration.js
@@ -34,3 +34,19 @@ names.forEach((name, index) => console.log(`${index + 1}) ${name}`));
 const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 const evens = numbers.filter(number => number % 2 === 0); //add even numbers
 console.log(evens);
+
+//using map method to edit arrays
+const strings = ['1', '2', '3', '4', '5'];
+const numbers2 = strings.map(string => parseInt(string, 10))
+console.log(numbers2);//this will turn the strings array into numbers
+
+const fruits2 = ['apple', 'pear', 'cherry']; //this does what the first code did but much cleaner and quicker
+const capFruits = fruits2.map(fruit => fruit.toUpperCase());
+console.log(capFruits);
+const prices2 = [5, 4.23, 6.4, 8.09, 3.20];
+const priceToDollars = price => '$' + price.toFixed(2);
+const newPrices = prices2.map(priceToDollars)
+//const newPrices = prices2.map(price => `$${price.toFixed(2)}`);
+console.log(newPrices);
+// Result: [ '$5.00', '$4.23', '$6.40', '$8.09', '$3.20' ]
+
